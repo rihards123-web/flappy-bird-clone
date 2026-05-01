df53fd6857970400abb6c8728b2330f78c4db271
rihards123-web <rihardsnol@gmail.com>
2026-05-02 00:35:07 +0300

    adds a restart button, that resets the games state, and then the game... well - is reset :D

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,4 +1,6 @@
+using NUnit.Framework;
 using UnityEngine;
+using static UnityEngine.InputManagerEntry;
 
 public class GameController : MonoBehaviour
 {
@@ -18,11 +20,11 @@ public class GameController : MonoBehaviour
 
     [SerializeField] private Rigidbody2D birdRigidBody;
 
+    [SerializeField] private GameObject restartGameButton;
+
     public bool isGameOver;
 
     private float timer;
-
-  
 
     private void Awake()
     {
@@ -68,7 +70,33 @@ public class GameController : MonoBehaviour
         if (isGameOver)
         {
             birdRigidBody.constraints = RigidbodyConstraints2D.FreezeAll;
+
+            restartGameButton.SetActive(true);
         }
 
+    }
+
+    // attached to restart game button in the scene. 
+    public void RestartGame()
+    {
+        restartGameButton.SetActive(false);
+
+        birdRigidBody.transform.position = new Vector3(0, 0, 0);
+
+        birdRigidBody.constraints = RigidbodyConstraints2D.None;
+
+        // Find every active object in the current scene that has a PipeController component,
+        // then store those PipeController components in an array called existingPipes.
+
+        PipeController[] existingPipes = FindObjectsByType<PipeController>();
+
+        foreach (PipeController pipeController in existingPipes)
+        {
+            Destroy(pipeController.gameObject);
+        }
+
+        timer = 0f;
+        
+        isGameOver = false;
     }
 }
