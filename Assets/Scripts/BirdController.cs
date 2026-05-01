8d3f135ecc0e20481c322a8186220b72c0c1e620
rihards123-web <rihardsnol@gmail.com>
2026-05-01 16:39:29 +0300

    Updates jump logic - previosulyt when holding space continuous force was applied. this has been addressed by switching force mode to Impulse from Force.

--- Assets/Scripts/BirdController.cs
+++ Assets/Scripts/BirdController.cs
@@ -7,16 +7,34 @@ public class BirdController : MonoBehaviour
 
     [SerializeField] private float jumpForce;
 
+    private bool shouldJump = false;
+
     // Update is called once per frame
     void Update()
+    {
+        isSpacePressed();
+    }
+
+    private void FixedUpdate()
     {
         Jump();
     }
 
     public void Jump()
     {
-        if (Keyboard.current.spaceKey.isPressed) {
-            birdRigidBody.AddForce(transform.up * jumpForce);
+        if (shouldJump == true)
+        {
+            birdRigidBody.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+
+            shouldJump = false;
+        }
+    }
+
+    private void isSpacePressed()
+    {
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+             shouldJump = true;
         }
     }
 }
